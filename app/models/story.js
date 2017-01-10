3eb629c6d50ae46c723a2c6e44acf606656668e6
Brian Pritt and Daniel Munger <daniel@>
2017-01-10 12:02:03 -0800

    add styling to story-tiles.

--- app/models/story.js
+++ app/models/story.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   headline: DS.attr(),
   author: DS.attr(),
-  headline: DS.attr(),
-  picture: DS.attr()
+  content: DS.attr(),
+  picture: DS.attr(),
+
 });
